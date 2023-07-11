74aa326cc5b0de8c1aa4cfc4609eb1ba0dd3affe
shubham1upadhyay <shubhamup47@gmail.com>
2023-07-12 01:38:46 +0530

    code added

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ const App = () => {
   }
   return (
     <div>
-        <p>Button clicked <span>{val}</span>times</p>
+        <p>Button clicked <span>{val}</span> times</p>
         <button onClick={counter}></button>
     </div>
   )
